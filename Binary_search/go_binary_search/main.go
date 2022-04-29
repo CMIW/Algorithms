9203a61a64e1074fabbf62b654ac4be36c634734
Ian <pbl9815@gmail.com>
2022-04-29 08:17:35 -0600

    Added generics to the Go Binary Search algorithm.

--- Binary_search/go_binary_search/main.go
+++ Binary_search/go_binary_search/main.go
@@ -1,16 +1,9 @@
 package main
 
-import (
-  "fmt"
-  "golang.org/x/exp/constraints"
-)
+import "golang.org/x/exp/constraints"
 
 func main() {
-    fmt.Println("Hello, World this is Binary Search!")
-    array := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17}
-    target := 14
 
-    fmt.Println(BinarySearch(array, target))
 }
 
 func BinarySearch[T constraints.Ordered](a []T, v T) bool{
