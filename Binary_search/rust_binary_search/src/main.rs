a56293822e0383354fdb5df60983adf0c412fc4c
medaka <medaka@pop-os.localdomain>
2023-11-26 11:24:18 -0600

    the rust binary search now returns the index of the found value and now works with negative numbers.

--- Binary_search/rust_binary_search/src/main.rs
+++ Binary_search/rust_binary_search/src/main.rs
@@ -1,11 +1,8 @@
-#[macro_use]
-extern crate log;
-
 fn main() {
 
 }
 
-fn binary_search<N: Ord>(a: &[N], v: N) -> Option<N> {
+fn binary_search<N: Ord>(a: &[N], v: N) -> Option<usize> {
     let mut l = 0;
     let mut r = a.len() - 1;
     while l <= r {
@@ -14,10 +11,11 @@ fn binary_search<N: Ord>(a: &[N], v: N) -> Option<N> {
             l = m + 1;
         }
         else if a[m] > v {
+            if m == 0 { break; }
             r = m - 1;
         }
         else{
-            return Some(v);
+            return Some(m);
         }
     }
     None
@@ -27,15 +25,9 @@ fn binary_search<N: Ord>(a: &[N], v: N) -> Option<N> {
 mod tests {
     use super::*;
 
-    fn init() {
-        let _ = env_logger::builder().is_test(true).try_init();
-    }
-
     #[test]
     // Checking for a value that is in the array
     fn binary_search_passes() {
-        init();
-
         let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
         let target = 14;
 
@@ -45,10 +37,17 @@ mod tests {
     #[test]
     // Checking for a value that isn't in the array
     fn binary_search_fails(){
-        init();
-
         let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
         let target = 54;
+
+        assert_eq!(None, binary_search(&array, target));
+    }
+
+    #[test]
+    // Checking for a value that isn't in the array
+    fn binary_search_fails_negative(){
+        let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+        let target = -4;
 
         assert_eq!(None, binary_search(&array, target));
     }
